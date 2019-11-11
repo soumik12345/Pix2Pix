4767179932b588031a5e3986d18df8eaefa2766c
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-12 04:15:51 +0530

    Minor Bug Fix

--- src/dataset.py
+++ src/dataset.py
@@ -20,9 +20,9 @@ def visualize(image_file):
     plt.setp(axes.flat, xticks = [], yticks = [])
     for i, ax in enumerate(axes.flat):
         if i % 2 == 0:
-            ax.imshow(input_image)
+            ax.imshow(input_image.numpy())
             ax.set_xlabel('Input_Image')
         else:
-            ax.imshow(real_image)
+            ax.imshow(real_image.numpy())
             ax.set_xlabel('Real_Image')
     plt.show()
