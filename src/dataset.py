95fe1e0d966ce43770f49f918e590d739d042dce
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-12 04:24:46 +0530

    Added Dataset and Augmentation Utilites

--- src/dataset.py
+++ src/dataset.py
@@ -26,3 +26,40 @@ def visualize(image_file):
             ax.imshow(real_image.numpy() / 255.0)
             ax.set_xlabel('Real_Image')
     plt.show()
+
+
+def resize(input_image, real_image, height, width):
+    input_image = tf.image.resize(
+        input_image, [height, width],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+    )
+    real_image = tf.image.resize(
+        real_image, [height, width],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR
+    )
+    return input_image, real_image
+
+
+def random_crop(input_image, real_image):
+    stacked_image = tf.stack([input_image, real_image], axis=0)
+    cropped_image = tf.image.random_crop(
+        stacked_image,
+        size=[2, IMG_HEIGHT, IMG_WIDTH, 3]
+    )
+    return cropped_image[0], cropped_image[1]
+
+
+def normalize(input_image, real_image):
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
+    return input_image, real_image
+
+
+@tf.function()
+def random_jitter(input_image, real_image):
+    input_image, real_image = resize(input_image, real_image, 286, 286)
+    input_image, real_image = random_crop(input_image, real_image)
+    if tf.random.uniform(()) > 0.5:
+        input_image = tf.image.flip_left_right(input_image)
+        real_image = tf.image.flip_left_right(real_image)
+    return input_image, real_image
