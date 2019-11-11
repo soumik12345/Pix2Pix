53586614a514dff676ebebf44fe81988212d4cd9
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-12 04:39:51 +0530

    Added Dataset Generation

--- src/dataset.py
+++ src/dataset.py
@@ -66,3 +66,34 @@ def random_jitter(input_image, real_image):
         input_image = tf.image.flip_left_right(input_image)
         real_image = tf.image.flip_left_right(real_image)
     return input_image, real_image
+
+
+def load_image_train(image_file):
+    input_image, real_image = load(image_file)
+    input_image, real_image = random_jitter(input_image, real_image)
+    input_image, real_image = normalize(input_image, real_image)
+    return input_image, real_image
+
+
+def load_image_test(image_file):
+    input_image, real_image = load(image_file)
+    input_image, real_image = resize(
+        input_image, real_image,
+        IMG_HEIGHT, IMG_WIDTH
+    )
+    input_image, real_image = normalize(input_image, real_image)
+    return input_image, real_image
+
+
+def get_datasets(path):
+    train_dataset = tf.data.Dataset.list_files(path + 'train/*.jpg')
+    train_dataset = train_dataset.map(
+        load_image_train,
+        num_parallel_calls=tf.data.experimental.AUTOTUNE
+    )
+    train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+    train_dataset = train_dataset.batch(BATCH_SIZE)
+    test_dataset = tf.data.Dataset.list_files(path + 'test/*.jpg')
+    test_dataset = test_dataset.map(load_image_test)
+    test_dataset = test_dataset.batch(BATCH_SIZE)
+    return train_dataset, test_dataset
