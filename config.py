ae8fe630f2093d95043929a265ec39b7e606c10f
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-12 06:24:09 +0530

    Added Checkpoints

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ IMG_WIDTH = 256
 IMG_HEIGHT = 256
 OUTPUT_CHANNELS = 3
 LAMBDA = 100
+EPOCHS = 150
 EXISTING_DATASETS = {
     'cityscapes' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/cityscapes.tar.gz',
     'edges2handbags' : 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2handbags.tar.gz',
--- src/train.py
+++ src/train.py
@@ -1,3 +1,5 @@
+from os.path import join
+from tensorflow.train import Checkpoint
 from tensorflow.keras.optimizers import Adam
 
 
@@ -5,3 +7,13 @@ def get_optimizers():
     generator_optimizer = Adam(2e-4, beta_1=0.5)
     discriminator_optimizer = Adam(2e-4, beta_1=0.5)
     return discriminator_optimizer, generator_optimizer
+
+
+def get_checkpoint(discriminator_optimizer, generator_optimizer, checkpoint_dir='./training_checkpoints'):
+    checkpoint_prefix = join(checkpoint_dir, 'ckpt')
+    checkpoint = tf.train.Checkpoint(
+        generator_optimizer=generator_optimizer,
+        discriminator_optimizer=discriminator_optimizer,
+        generator=generator, discriminator=discriminator
+    )
+    return checkpoint
