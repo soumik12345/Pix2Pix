5b071f6f8a11c0645b9c9de146ee5546cc84c9bb
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-16 23:48:50 +0530

    Added Mish Activation Function

--- src/blocks.py
+++ src/blocks.py
@@ -1,10 +1,12 @@
+import tensorflow as tf
 from tensorflow import random_normal_initializer
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import (
-    Conv2D, LeakyReLU,
+    Conv2DTranspose, Dropout,
     BatchNormalization, ReLU,
-    Conv2DTranspose, Dropout
+    Conv2D, LeakyReLU, Activation
 )
+from tensorflow.keras.utils.generic_utils import get_custom_objects
 
 
 def downsample_block(filters, kernel_size, batch_norm = True):
@@ -52,3 +54,32 @@ def upsample_block(filters, kernel_size, dropout = False):
         block.add(Dropout(0.5))
     block.add(ReLU())
     return block
+
+
+
+class Mish(Activation):
+    '''
+    Code Credits: https://github.com/digantamisra98/Mish
+    Mish Activation Function.
+    .. math::
+        mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + e^{x}))
+    Shape:
+        - Input: Arbitrary. Use the keyword argument `input_shape`
+        (tuple of integers, does not include the samples axis)
+        when using this layer as the first layer in a model.
+        - Output: Same shape as the input.
+    Examples:
+        >>> X = Activation('Mish', name="conv1_act")(X_input)
+    '''
+
+    def __init__(self, activation, **kwargs):
+        super(Mish, self).__init__(activation, **kwargs)
+        self.__name__ = 'Mish'
+
+
+def mish(inputs):
+    '''Code Credits: https://github.com/digantamisra98/Mish
+    '''
+    return inputs * tf.math.tanh(tf.math.softplus(inputs))
+
+get_custom_objects().update({'Mish': Mish(mish)})
