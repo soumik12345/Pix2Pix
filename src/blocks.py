f19b0c11657fea18ee01529c538da5320105ae63
Soumik Rakshit <19soumik.rakshit96@gmail.com>
2019-11-17 02:23:48 +0530

    Modified Activation Control

--- src/blocks.py
+++ src/blocks.py
@@ -10,8 +10,8 @@ from tensorflow.keras.layers import (
 from tensorflow.keras.utils import get_custom_objects
 
 
-def downsample_block(filters, kernel_size, batch_norm = True):
-    '''Pix2Pix Downsample Block
+def downsample_block_generator(filters, kernel_size, batch_norm = True, use_config_activation = False):
+    '''Pix2Pix Downsample Block for building the generator
     Reference: https://arxiv.org/abs/1611.07004
     Params:
         filters     -> Number of filters in the convolution layer
@@ -29,14 +29,40 @@ def downsample_block(filters, kernel_size, batch_norm = True):
     )
     if batch_norm:
         block.add(BatchNormalization())
-    if ACTIVATION != '':
+    if use_config_activation:
         block.add(Activation(ACTIVATION))
     else:
         block.add(LeakyReLU())
     return block
 
 
-def upsample_block(filters, kernel_size, dropout = False):
+def downsample_block_discriminator(filters, kernel_size, batch_norm = True, use_config_activation = False):
+    '''Pix2Pix Downsample Block for building the generator
+    Reference: https://arxiv.org/abs/1611.07004
+    Params:
+        filters     -> Number of filters in the convolution layer
+        kernel_size -> Sizer of convolution kernel
+        batch_norm  -> Apply BatchNormalization (Flag)
+    '''
+    initializer = random_normal_initializer(0.0, 0.02)
+    block = Sequential()
+    block.add(
+        Conv2D(
+            filters, kernel_size,
+            strides=2, padding='same',
+            kernel_initializer=initializer, use_bias=False
+        )
+    )
+    if batch_norm:
+        block.add(BatchNormalization())
+    if use_config_activation:
+        block.add(Activation(ACTIVATION))
+    else:
+        block.add(LeakyReLU())
+    return block
+
+
+def upsample_block(filters, kernel_size, dropout = False, use_config_activation = False):
     '''Pix2Pix Upsample Block
     Reference: https://arxiv.org/abs/1611.07004
     Params:
@@ -56,7 +82,7 @@ def upsample_block(filters, kernel_size, dropout = False):
     block.add(BatchNormalization())
     if dropout:
         block.add(Dropout(0.5))
-    if ACTIVATION != '':
+    if use_config_activation:
         block.add(Activation(ACTIVATION))
     else:
         block.add(ReLU())
